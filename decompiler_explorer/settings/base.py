b20d5af2e80cea7aa1c1180514c02a72f2437bf8
ahmedmhiri1 <yahyamhiri55@gmail.com>
2026-03-20 10:17:25 +0100

    Configure upload memory size limits
    Set maximum upload memory size for data and files.

--- decompiler_explorer/settings/base.py
+++ decompiler_explorer/settings/base.py
@@ -17,7 +17,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
-
+DATA_UPLOAD_MAX_MEMORY_SIZE = 20971520 
+FILE_UPLOAD_MAX_MEMORY_SIZE = 20971520
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -204,3 +205,6 @@ if DEBUG:
         "127.0.0.1",
         "dce.local",
     ]
+
+DATA_UPLOAD_MAX_MEMORY_SIZE = 20971520 
+FILE_UPLOAD_MAX_MEMORY_SIZE = 20971520
